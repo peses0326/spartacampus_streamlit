0b8fd0fb7b3110d98f016c3d7b9a7911d0ed9d6c
Gorokke <80080041+peses0326@users.noreply.github.com>
2025-04-07 13:01:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
+import os
+from openai import OpenAI
 import streamlit as st
 
-st.title("동물 이미지 찾기")
+os.environ["OPENAI_API_KEY"] = st.secrets['OPEN_API_KEY']
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
+st.title('슈퍼 시나리오 봇🥸')
 
-title = st.text_input("동물 이름을 입력해주세요")
+keyword = st.text_input("키워드를 입력하세요")
 
-if st.button("검색"):
-    url = f'https://edu.spartacodingclub.kr/random/?{title} '
-    st.image(url)
+if st.button('생성하기'):
+    with st.spinner('생성 중입니다.'):
+        chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": keyword,
+                },
+                {
+                    "role": "system",
+                    "content": "입력 받은 키워드에 대한 흥미진진한 300자 이내의 시나리오를 작성해줘.",
+                }
+            ],
+            model="gpt-4o",
+        )
 
-
-
-
+    result = chat_completion.choices[0].message.content
+    st.write(result)
